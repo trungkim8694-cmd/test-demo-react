80aab69002feddd7dc98f17a03528fb69efb59c7
trung kim <trungkim8694@gmail.com>
2025-11-29 18:50:11 +0700

    video 24

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -3,12 +3,19 @@ import React from "react";
 class DisplayInfo extends React.Component {
   render() {
     // destructuring array/ object
-    const { age, name } = this.props;
+    const { listUsers } = this.props; //tương dudong với const lisUsers = this.props.listUsers;
     //props => viết tắt của từ property
     return (
       <div>
-        <div>My name's {name}</div>
-        <div>My age's {age}</div>
+        {listUsers.map((user, index) => {
+          return (
+            <div key={user.id}>
+              <div>My name's {user.name}</div>
+              <div>My age's {user.age}</div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,6 +6,13 @@ import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "hoi dan it", age: "30" },
+      { id: 2, name: "Eric", age: "26" },
+      { id: 3, name: "HaryPhamDev", age: "69" },
+    ],
+  };
   // JSX cú pháp đặc biết react
   render() {
     return (
@@ -13,9 +20,7 @@ class MyComponent extends React.Component {
         <UserInfo />
         <br />
         <br />
-        <DisplayInfo name="Hoi dan IT" age="30" />
-        <hr />
-        <DisplayInfo name="Eric" age="26" />
+        <DisplayInfo listUsers={this.state.listUsers} />
       </div>
     );
   }
